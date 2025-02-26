4466fb49560b1ccfc92de34e5062050c5bb904bc
Pogsthegreatest <kentmanalo85@gmail.com>
2025-02-26 23:25:05 +0800

    Add files via upload

---
+++ view.php
@@ -0,0 +1,31 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "esp32_data"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die(json_encode(["status" => "error", "message" => "Connection failed: " . $conn->connect_error]));
+}
+
+$sql = "SELECT * FROM temperature_logs"; 
+$result = $conn->query($sql);
+
+$records = [];
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $records[] = $row;
+    }
+    echo json_encode(["status" => "success", "data" => $records]);
+} else {
+    echo json_encode(["status" => "error", "message" => "No records found"]);
+}
+
+$conn->close();
+?>
